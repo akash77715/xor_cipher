b600ad78b30d704020409fc084dc8349b0a0a92b
Akash77715 <ag28796@gmail.com>
2020-08-21 20:11:08 +0530

    Code View Added!

--- js/script_function.js
+++ js/script_function.js
@@ -4,6 +4,7 @@
 	var key2;
 	var enc_str="";
 	var dec_str="";
+	var view=1;
 	function store(){
 		instr=document.getElementById('in_str').value.trim();
 		var letters = /^[A-Za-z]+$/;
@@ -24,6 +25,20 @@
 			document.getElementById("btn2").style.display="block";
 			document.getElementById("keylabel").style.display="block";
 			document.getElementById("inchar").style.display="block";
+			
+			//animating code view
+			document.getElementById("loc07").style.color="black";
+			document.getElementById("loc07").style.fontWeight="normal";
+			document.getElementById("loc07").style.background="whitesmoke";
+			document.getElementById("loc08").style.color="black";
+			document.getElementById("loc08").style.fontWeight="normal";
+			document.getElementById("loc08").style.background="whitesmoke";
+			document.getElementById("loc09").style.color="blue";
+			document.getElementById("loc09").style.fontWeight="bold";
+			document.getElementById("loc09").style.background="pink";
+			document.getElementById("loc10").style.color="blue";
+			document.getElementById("loc10").style.fontWeight="bold";
+			document.getElementById("loc10").style.background="pink";
 			
 			
 		}
@@ -55,6 +70,34 @@
 			for(i=0;i<instr.length;i++){
 				document.getElementById('eqlt').innerHTML=document.getElementById('eqlt').innerHTML+"<td class='eqlbox'>=<td>";
 			}
+			//animating code view
+			document.getElementById("loc09").style.color="black";
+			document.getElementById("loc09").style.fontWeight="normal";
+			document.getElementById("loc09").style.background="whitesmoke";
+			document.getElementById("loc10").style.color="black";
+			document.getElementById("loc10").style.fontWeight="normal";
+			document.getElementById("loc10").style.background="whitesmoke";
+			document.getElementById("loc12").style.background="green";
+			document.getElementById("loc12").style.fontWeight="bold";
+			document.getElementById("loc12").style.color="white";
+			document.getElementById("loc21").style.background="lightgrey";
+			document.getElementById("loc22").style.background="lightgrey";
+			document.getElementById("loc23").style.background="lightgrey";
+			document.getElementById("loc24").style.background="lightgrey";
+			document.getElementById("loc25").style.background="lightgrey";
+			document.getElementById("loc26").style.background="lightgrey";
+			document.getElementById("loc14").style.color="blue";
+			document.getElementById("loc14").style.fontWeight="bold";
+			document.getElementById("loc14").style.background="pink";
+			document.getElementById("loc15").style.color="blue";
+			document.getElementById("loc15").style.fontWeight="bold";
+			document.getElementById("loc15").style.background="pink";
+			document.getElementById("loc16").style.color="blue";
+			document.getElementById("loc16").style.fontWeight="bold";
+			document.getElementById("loc16").style.background="pink";
+			document.getElementById("loc17").style.color="blue";
+			document.getElementById("loc17").style.fontWeight="bold";
+			document.getElementById("loc17").style.background="pink";
 			
 			encrypt();
 	}
@@ -133,16 +176,57 @@
 			document.getElementById("out6").style.display="block";
 			
 			document.getElementById("out6").style.color = 'red';
+			
+			//animating code view
+			document.getElementById("loc14").style.color="black";
+			document.getElementById("loc14").style.fontWeight="normal";
+			document.getElementById("loc14").style.background="whitesmoke";
+			document.getElementById("loc15").style.color="black";
+			document.getElementById("loc15").style.fontWeight="normal";
+			document.getElementById("loc15").style.background="whitesmoke";
+			document.getElementById("loc16").style.color="black";
+			document.getElementById("loc16").style.fontWeight="normal";
+			document.getElementById("loc16").style.background="whitesmoke";
+			document.getElementById("loc17").style.color="black";
+			document.getElementById("loc17").style.fontWeight="normal";
+			document.getElementById("loc17").style.background="whitesmoke";
+			document.getElementById("loc18").style.color="white";
+			document.getElementById("loc18").style.fontWeight="bold";
+			document.getElementById("loc18").style.background="red";
+			
 			if(instr==dec_str){
 				document.getElementById('in_str2').disabled = true;
 				document.getElementById('key2').disabled = true;
 				document.getElementById('btndec').disabled = true;
 				document.getElementById('btndec').style.backgroundColor = 'grey';
 				document.getElementById("out6").style.color = 'green';
+				
+				//animating code view
+				document.getElementById("loc18").style.background="green";
+				document.getElementById("loc18").style.color="white";
+				
+				document.getElementById("loc21").style.background="lightgreen";
+				document.getElementById("loc22").style.background="lightgreen";
+				document.getElementById("loc23").style.background="lightgreen";
+				document.getElementById("loc24").style.background="lightgreen";
+				document.getElementById("loc25").style.background="lightgreen";
+				document.getElementById("loc26").style.background="lightgreen";
 			}
 			
 		}
 		else{
 			alert("Invalid length!");
+		}
+	}
+	function view_change(){
+		if(view==1){
+			document.getElementById("logical").style.display="none";
+			document.getElementById("code").style.display="block";
+			view=0;
+		}
+		else{
+			document.getElementById("code").style.display="none";
+			document.getElementById("logical").style.display="block";
+			view=1;
 		}
 	}
